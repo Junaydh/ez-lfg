1aeebe90e832dec213ece199addc91e0c3841c46
Junayd H <junaydhus@gmail.com>
2023-03-21 13:32:58 -0700

    create schema dir and users schema file

---
+++ backend/db/schema/1_users.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    profile_pic VARCHAR(255) DEFAULT 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973461_960_720.png',
+    discord_tag VARCHAR(255) NOT NULL,
+);
