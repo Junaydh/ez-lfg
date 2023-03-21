154f19d1d2c3522338358a48af87ce859738ca39
Junayd H <junaydhus@gmail.com>
2023-03-21 13:50:10 -0700

    create participants schema

---
+++ backend/db/schema/4_sessions_users.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS sessions_users CASCADE;
+CREATE TABLE sessions_users (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    session_id INTEGER NOT NULL REFERENCES sessions(id) ON DELETE CASCADE,
+);
