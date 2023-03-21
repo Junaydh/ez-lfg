62869cbdc7f739227428b0388a2ad11f78c25930
Adam-Marx <adamalexmarx@gmail.com>
2023-03-21 16:49:54 -0700

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { handleClick } from './hooks/test'
+import Navbar from './components/Nav';
 
 function App() {
   return (
-    <div>
+    <main>
+      <Navbar />
       <button onClick={handleClick}>Test Request</button>
       <h1>EZLFG</h1>
-    </div>
+    </main>
   );
 }
 
