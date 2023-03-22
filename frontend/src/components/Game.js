8b4547b424ed8dc0f7544bff19564d5f9a2f0335
Colton Hyland <colthyland@gmail.com>
2023-03-22 03:00:03 -0700

    GamesList has a search bar which filters the games in the db

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Game.scss';
 
-export default Game = (props) => {
+export default function Game (props) {
   <div className="game">
     <img className="game__image" src={props.img} alt={props.name} />
     <span className="game__name">{props.name}</span>
--- frontend/src/components/GamesList.js
+++ frontend/src/components/GamesList.js
@@ -4,6 +4,17 @@ import Game from './Game';
 import './GamesList.scss';
 
 const GamesList = (props) => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    axios.get('/games')
+      .then(response => setGames(response.data))
+      .catch(error => console.log('Error fetching games:', error));
+  }, []);
+
+
+  const filteredGames = games.filter(game => game.name.toLowerCase().includes(searchQuery.toLowerCase()));
 
   return (
     <div className="games-list">
@@ -11,9 +22,13 @@ const GamesList = (props) => {
         className="games-list__search"
         type="text"
         placeholder="Search for a game"
+        value={searchQuery}
+        onChange={(event) => setSearchQuery(event.target.value)}
       />
       <div className="games-list__games">
-          <Game />
+        {filteredGames.map(game => (
+          <Game key={game.id} name={game.name} image={game.image} />
+        ))}
       </div>
     </div>
   );
