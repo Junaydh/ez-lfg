7880c1500ff88beaf53bdf7a35ffb9f664301b52
Colton Hyland <colthyland@gmail.com>
2023-03-22 11:41:31 -0700

    Pulling new features

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import { handleClick } from './hooks/test'
 import Navbar from './components/Navbar';
 import GamesList from './components/GamesList';
+import FilterBar from './components/Filterbar';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
       <h1>EZLFG</h1>
       <div>
         <GamesList />
+      </div>
+      <div>
+        <FilterBar />
       </div>
     </main>
   );
