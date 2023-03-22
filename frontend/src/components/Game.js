5960225c0e42725dcd3e5787f01063eb48721154
Colton Hyland <colthyland@gmail.com>
2023-03-22 07:17:33 -0700

    Gameslist shows games in an order

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Game.scss';
 
-export default function Game (props) {
-  <div className="game">
-    <img className="game__image" src={props.img} alt={props.name} />
-    <span className="game__name">{props.name}</span>
-  </div>
+const Game = (props) => {
+  return (
+    <div className="game">
+      <img className="game__logo" src={props.logo} alt={props.name} />
+      <span className="game__name">{props.name}</span>
+    </div>
+  );
 };
+
+export default Game;
--- frontend/src/components/GamesList.js
+++ frontend/src/components/GamesList.js
@@ -3,39 +3,22 @@ import axios from 'axios';
 import './GamesList.scss';
 import Game from './Game';
 
-const GamesList = (props) => {
-  const [searchQuery, setSearchQuery] = useState('');
+const GamesList = () => {
   const [games, setGames] = useState([]);
-  const [filteredGames, setFilteredGames] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:3001/games')
       .then(response => {
         setGames(response.data);
-        setFilteredGames(response.data);
       })
       .catch(error => console.log('Error fetching games:', error));
   }, []);
 
-  const handleSearch = (event) => {
-    const query = event.target.value.toLowerCase();
-    const filtered = games.filter(game => game.name.toLowerCase().includes(query));
-    setFilteredGames(filtered);
-    setSearchQuery(query);
-  };
-
   return (
     <div className="games-list">
-      <input
-        className="games-list__search"
-        type="text"
-        placeholder="Search for a game"
-        value={searchQuery}
-        onChange={handleSearch}
-      />
       <div className="games-list__games">
-        {filteredGames.map(game => (
-          <Game key={game.id} game={game} onClick={() => console.log(`Clicked game ${game.name}`)} />
+        {games.map(game => (
+          <Game key={game.id} logo={game.game_logo} name={game.name} />
         ))}
       </div>
     </div>
