70764b532c973c36ec46856bdf9e86bc523b2a04
Colton Hyland <colthyland@gmail.com>
2023-03-21 17:20:30 -0700

    fixed typo'

--- backend/db/seeds/2_sessions_seeds.sql
+++ backend/db/seeds/2_sessions_seeds.sql
@@ -6,4 +6,4 @@ VALUES (1, 1, 'NA', 'Valorant Ranked', 'Looking for players to grind Valorant Ra
 (3, 3, 'NA', 'League of Legends Flex', 'Creating a team for LoL Flex', 5, 2, TRUE, TRUE, 'https://discord.gg/lolflex', 'PC'),
 (4, 4, 'NA', 'Overwatch Comp', 'Looking for players to play Overwatch Comp with', 6, 4, TRUE, TRUE, 'https://discord.gg/overwatchcomp', 'PlayStation'),
 (5, 5, 'EU', 'Apex Legends Ranked', 'Need a full team for Apex Legends Ranked', 3, 2, TRUE, TRUE, 'https://discord.gg/apexranked', 'PC'),
-(6, 6, 'NA', 'Minecraft Casual Survival', 'Looking for players to play some Minecraft Survival together', 10, 5, FALSE, FALSE, 'https://discord.gg/minecraftsurvival', 'PC'),
+(6, 6, 'NA', 'Minecraft Casual Survival', 'Looking for players to play some Minecraft Survival together', 10, 5, FALSE, FALSE, 'https://discord.gg/minecraftsurvival', 'PC');
