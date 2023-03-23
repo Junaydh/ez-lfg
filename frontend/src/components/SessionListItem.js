03e40afd79f5509d37d1f28dda4a4f2f91f78646
Adam-Marx <adamalexmarx@gmail.com>
2023-03-22 19:16:56 -0700

    Should have commit more. Logic to display all session details, using back end routes to fetch database data

--- frontend/src/components/SessionListItem.js
+++ frontend/src/components/SessionListItem.js
@@ -1,29 +1,47 @@
 import './SessionListItem.scss';
+import { getSessionPlayers } from '../hooks/getSessionPlayers';
+import { useState, useEffect } from 'react';
 
-function SessionListItem({session}) {
+function SessionListItem({ session }) {
+  const [sessionPlayers, setSessionPlayers] = useState([]);
+
+  useEffect(() => {
+    getSessionPlayers(session.id).then(players => {
+      setSessionPlayers(players);
+    });
+  }, [session.id]);
 
   const date = new Date(session.created_at);
-  const formattedDate = date.toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    hour12: true
+  const formattedDate = date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: true,
   });
+
+  const playersUsernames = sessionPlayers.map((player) => player.username);
 
   return (
     <div key={session.id} className="session-card">
       <h2>{session.title}</h2>
       <h3>Host: {session.creator.username}</h3>
-      <h5><i className="fab fa-discord"></i> {session.creator.discord_tag}</h5>
-      <div className='session-prefs-description'>  
-        <div className='preferences'>
-          <span>Players: 2/{session.max_players}</span>
-          <span>Mic Required: {session.mic_required ? 'Yes' : 'No'}</span>
+      <h5>
+        <i className="fab fa-discord"></i> {session.creator.discord_tag}
+      </h5>
+      <div className="session-prefs-description">
+        <div className="preferences">
+          <span>
+            Players: {session.users.length}/{session.max_players}
+          </span>
+          <span>Mic Required: {session.mic_required ? "Yes" : "No"}</span>
         </div>
         <p>{session.description}</p>
+        <div>
+          <span>Players: {playersUsernames.join(", ")}</span>
+        </div>
       </div>
       <span>{formattedDate}</span>
     </div>
