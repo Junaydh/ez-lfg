a458f89636e5c936825b5ffdb663b787330727d0
Adam-Marx <adamalexmarx@gmail.com>
2023-03-22 19:30:10 -0700

    General naming scheme changes that make more sense

---
+++ backend/routes/sessionsApi.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const sessionsSelector = require('../db/queries/sessionsSelector');
+const usersSelector = require('../db/queries/usersSelector');
+const sessionSelector = require('../db/queries/sessionSelector');
+
+router.get('/', (req, res) => {
+  sessionsSelector.findAll()
+  .then(rows => {
+    res.json(rows);
+  })
+  .catch (err => {
+    console.error(err.message);
+  });
+})
+
+router.get('/user/:id', (req, res) => {
+  usersSelector.findUser(req.params.id)
+   .then(rows => {
+     res.json(rows);
+   })
+   .catch (err => {
+     console.error(err.message);
+   });
+})
+
+router.get('/session/:id/users', (req, res) => {
+  sessionSelector.findSession(req.params.id)
+   .then(rows => {
+     res.json(rows);
+   })
+   .catch (err => {
+     console.error(err.message);
+   });
+})
+
+
+ 
+
+module.exports = router;
