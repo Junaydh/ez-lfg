b62a38c7383f783779c79692a5d74c7b80df8fcb
Junayd H <junaydhus@gmail.com>
2023-03-22 21:04:21 -0400

    create login, logout, and register routes

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,18 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require("cors");
+const session = require('express-session');
 const passport = require('passport');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const initializePassport = require('./auth/passport-config');
 const db = require('./db');
+
+initializePassport(passport);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const testRouter = require('./routes/test');
+const registrationRouter = require('./routes/registration');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
@@ -26,11 +29,52 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
 app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/test', testRouter);
+// Registration route
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const createUserQuery = `
+      INSERT INTO users (username, password, email, profile_pic, discord_tag)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING id, username, email, profile_pic, discord_tag
+    `;
+    const createUserResult = await db.query(createUserQuery, [req.body.username, hashedPassword, req.body.email, req.body.profile_pic, req.body.discord_tag]);
+    const user = createUserResult.rows[0];
+    req.login(user, (error) => {
+      if (error) {
+        return next(error);
+      }
+      return res.redirect('/');
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+//login route
+app.post('/login', passport.authenticate('local'), (req, res) => {
+  res.json(req.user);
+});
+
+//logout route
+app.post('/logout', (req, res) => {
+  req.logout();
+  res.status(200).json({ message: 'Logged out successfully' });
+  }
+);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
