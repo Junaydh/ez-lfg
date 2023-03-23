5c18c6f05c8668820ecb281d1339e6c052dfd5c5
Adam-Marx <adamalexmarx@gmail.com>
2023-03-22 19:30:59 -0700

    Add get requests to individual session and host of session to display in SessionListItem

--- frontend/src/hooks/useSessions.js
+++ frontend/src/hooks/useSessions.js
@@ -5,13 +5,21 @@ export function useSessions() {
   const [sessions, setSessions] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/test/sessions')
+    axios.get('http://localhost:3001/sessions')
       .then(response => {
         const newSessions = response.data.map(session => {
-          return axios.get(`http://localhost:3001/test/user/${session.creator_id}`).then(response => {
-            session.creator = response.data[0];
-            return session;
-          });
+          return axios.get(`http://localhost:3001/sessions/user/${session.creator_id}`)
+            .then(response => {
+              session.creator = response.data[0];
+              return session;
+            })
+            .then(session => {
+              return axios.get(`http://localhost:3001/sessions/session/${session.id}/users`)
+                .then(response => {
+                  session.users = response.data;
+                  return session;
+                });
+            });
         });
         Promise.all(newSessions).then(sessionsWithCreator => {
           setSessions(sessionsWithCreator);
