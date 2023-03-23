2503d41d035b9283a2dbc310a435cd7d394d1f13
Junayd H <junaydhus@gmail.com>
2023-03-22 21:09:53 -0400

    create helper functions to use in user auth components

---
+++ frontend/src/services/auth.js
@@ -0,0 +1,51 @@
+const API_URL = '/api/'; // Replace with your server's API URL
+
+async function register(username, password, email) {
+  const response = await fetch(`${API_URL}register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, password, email }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Registration failed');
+  }
+
+  const user = await response.json();
+  return user;
+}
+
+async function login(username, password) {
+  const response = await fetch(`${API_URL}login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, password }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Login failed');
+  }
+
+  const user = await response.json();
+  return user;
+}
+
+async function logout() {
+  const response = await fetch(`${API_URL}logout`, {
+    method: 'POST',
+  });
+
+  if (!response.ok) {
+    throw new Error('Logout failed');
+  }
+}
+
+export default {
+  register,
+  login,
+  logout,
+};
