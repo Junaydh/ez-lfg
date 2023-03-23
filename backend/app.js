06b359ce2c33930fa5b5f0cc477f9a8483f326ab
Adam-Marx <adamalexmarx@gmail.com>
2023-03-22 19:12:09 -0700

    Refactor test route naming scheme to sessions naming scheme

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const testRouter = require('./routes/test');
+const sessionsRouter = require('./routes/sessionsApi');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/test', testRouter);
+app.use('/sessions', sessionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
