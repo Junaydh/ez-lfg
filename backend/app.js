d0e9f64b513919d8d103dc66fc3ef60da5c898a1
Colton Hyland <colthyland@gmail.com>
2023-03-24 14:45:47 -0700

    include auth route

--- backend/app.js
+++ backend/app.js
@@ -6,44 +6,24 @@ const logger = require('morgan');
 const cors = require("cors");
 const indexRouter = require('./routes/index');
 const gamesRouter = require('./routes/gamesApi');
-
-const app = express();   
-var usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
 const sessionsRouter = require('./routes/sessionsApi');
+const authRouter = require('./routes/auth');
 
-
-
-
-const session = require('express-session');
-const pgSession = require('connect-pg-simple')(session);
-
-app.use(session({
-    store: new pgSession({
-        pool: db, // this should be your PostgreSQL database connection pool
-        tableName: 'session',
-        schemaName: 'public'
-    }),
-    secret: 'your-secret-key-here',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
-    }
-}));
+const app = express(); 
 
 
 
 
 
+// Set up routes
+app.use('/', indexRouter);
+app.use('/games', gamesRouter);
+app.use('/users', usersRouter);
+app.use('/sessions', sessionsRouter);
+app.use('/auth', authRouter);
 
-
-
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+// Set up middleware
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -51,24 +31,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use('/', indexRouter);
-app.use('/games', gamesRouter);
-app.use('/users', usersRouter);
-app.use('/sessions', sessionsRouter);
-
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
