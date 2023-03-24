eb906d1df9f70149134e2da9d2f8091673ab71c3
Colton Hyland <colthyland@gmail.com>
2023-03-24 14:47:21 -0700

    Route posts user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,37 +2,21 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const db = require('../db');
-const { check, validationResult } = require('express-validator');
+const { authenticateToken } = require('../authMiddleware');
 
-// User registration route
-router.post('/signup', [
-  check('username').notEmpty().withMessage('Username is required'),
-  check('password').notEmpty().withMessage('Password is required'),
-  check('email').isEmail().withMessage('Invalid email')
-], async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-  }
-
-  const { username, password, email } = req.body;
-
+// Signup route
+router.post('/signup', async function(req, res, next) {
   try {
-      // Check if username or email already exist
-      const userExists = await db.query('SELECT * FROM users WHERE username=$1 OR email=$2', [username, email]);
-      if (userExists.rows.length > 0) {
-          return res.status(400).json({ error: 'Username or email already exists' });
-      }
 
-      // Hash password
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
+    // Add user to database
+    const { rows } = await db.query('INSERT INTO users (username, password, email, discord_tag) VALUES ($1, $2, $3, $4) RETURNING *', [req.body.username, hash, req.body.email, req.body.discord_tag]);
 
-      // Insert new user into database
-      await db.query('INSERT INTO users (username, password, email) VALUES ($1, $2, $3)', [username, hashedPassword, email]);
-
-      res.json({ message: 'User registered successfully' });
+    res.status(201).json({
+      message: 'User created successfully',
+      user: rows[0]
+    });
   } catch (err) {
-      next(err);
+    console.error(err);
+    next(err);
   }
 });
