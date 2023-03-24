76b6c2f6a50d377da8528388db977f6e89efa85e
Colton Hyland <colthyland@gmail.com>
2023-03-24 14:48:02 -0700

    Checks username or if email exists

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,15 @@ const { authenticateToken } = require('../authMiddleware');
 // Signup route
 router.post('/signup', async function(req, res, next) {
   try {
+    // Check if user with same username or email exists
+    const { rows: existingUsers } = await db.query('SELECT * FROM users WHERE username = $1 OR email = $2', [req.body.username, req.body.email]);
+    if (existingUsers.length > 0) {
+      return res.status(400).json({ message: 'User with that username or email already exists' });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
 
     // Add user to database
     const { rows } = await db.query('INSERT INTO users (username, password, email, discord_tag) VALUES ($1, $2, $3, $4) RETURNING *', [req.body.username, hash, req.body.email, req.body.discord_tag]);
