e2abd02589d8757575704ccadc5853fef2012fdf
Colton Hyland <colthyland@gmail.com>
2023-03-25 12:26:02 -0700

    Userlist item displays user info. User list manages state of players in the session and displays them

---
+++ frontend/src/components/UserList.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { getSessionPlayers } from './getSessionPlayers';
+import UserListItem from './UserListItem';
+
+function UserList({ sessionId, userId }) {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    getSessionPlayers(sessionId).then(players => setPlayers(players));
+  }, [sessionId]);
+
+  // Only show the list of users if the current user is in the session
+  if (players.some(player => player.id === userId)) {
+    return (
+      <div className="user-list-container">
+        <h3>Players in this session:</h3>
+        <div className="user-list">
+          {players.map(player => (
+            <UserListItem key={player.id} user={player} />
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
---
+++ frontend/src/components/UserListItem.js
@@ -0,0 +1,9 @@
+function UserListItem({ user }) {
+  return (
+    <div className="user-list-item">
+      <img className="profile-pic" src={user.profile_pic} alt={`${user.username}'s profile pic`} />
+      <div className="username">{user.username}</div>
+      <div className="discord-tag">{user.discord_tag}</div>
+    </div>
+  );
+}
