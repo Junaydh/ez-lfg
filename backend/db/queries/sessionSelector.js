b061afda5ed398a8de01df7653c7323170ced2db
Adam-Marx <adamalexmarx@gmail.com>
2023-03-24 17:09:47 -0700

    Join session (post) and leave(delete) session queries

--- backend/db/queries/sessionSelector.js
+++ backend/db/queries/sessionSelector.js
@@ -10,29 +10,38 @@ const findSession  = (session) => {
   return db
   .query(queryString, queryParams)
   .then(data => {
-    console.log(data.rows)
     return data.rows;
   })
   .catch(err => {
     console.error(err.message);
   });
 }
-
 const addUserToSession = (userId, sessionId) => {
   const queryParams = [userId, sessionId];
-  const queryString = `INSERT INTO sessions_users (user_id, session_id)
-  VALUES ($1, $2)`;
+  const checkUserQuery = 'SELECT * FROM sessions_users WHERE user_id = $1 AND session_id = $2';
+  const insertUserQuery = 'INSERT INTO sessions_users (user_id, session_id) VALUES ($1, $2)';
 
-  return db
-  .query(queryString, queryParams)
-  .then(data => {
-    console.log(data.rows)
-    return data.rows;
-  })
-  .catch(err => {
-    console.error(err.message);
-  });
+  return db.query(checkUserQuery, queryParams)
+    .then(checkResult => {
+      if (checkResult.rows.length > 0) {
+        // user is already in the session
+        return null;
+      } else {
+        return db.query(insertUserQuery, queryParams)
+          .then(insertResult => insertResult.rows)
+          .catch(err => {
+            console.error(err.message);
+            throw err;
+          });
+      }
+    })
+    .catch(err => {
+      console.error(err.message);
+      throw err;
+    });
 }
+
+
 const findSessionsByGame = (gameId) => {
   const queryParams = [gameId];
   const queryString = `SELECT sessions.*, users.username AS creator_username, users.discord_tag AS creator_discord_tag
@@ -43,7 +52,6 @@ const findSessionsByGame = (gameId) => {
   return db
   .query(queryString, queryParams)
   .then(data => {
-    console.log(data.rows)
     return data.rows;
   })
   .catch(err => {
@@ -51,4 +59,22 @@ const findSessionsByGame = (gameId) => {
   });
 };
 
-module.exports = { findSession, findSessionsByGame, addUserToSession };
+const deleteUserFromSession = (userId, sessionId) => {
+  const queryParams = [userId, sessionId];
+  const queryString = `DELETE FROM sessions_users
+                        WHERE user_id = $1 AND session_id = $2
+                        RETURNING *`;
+
+  return db
+    .query(queryString, queryParams)
+    .then(data => {
+      return data.rows;
+    })
+    .catch(err => {
+      console.error(err.message);
+      throw err;
+    });
+}
+
+
+module.exports = { findSession, findSessionsByGame, addUserToSession, deleteUserFromSession };
