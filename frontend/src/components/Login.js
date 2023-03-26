05142db5f64eb352b92bcb49492f591d6638f0cf
Junayd H <junaydhus@gmail.com>
2023-03-26 16:38:12 -0400

    create basic login component

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { login } from '../services/auth';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await login(username, password);
+      console.log(user);
+      // Redirect to dashboard or home page
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
