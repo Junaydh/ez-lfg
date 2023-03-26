9fe0f71f29a7187a448585129bac40ec1fac65aa
Adam-Marx <adamalexmarx@gmail.com>
2023-03-26 13:45:21 -0700

    Form styling

--- frontend/src/components/SessionForm.js
+++ frontend/src/components/SessionForm.js
@@ -1,7 +1,10 @@
+import './SessionForm.scss';
 import React, { useState } from 'react';
 import { createSession } from '../hooks/createSession';
 
 const SessionForm = () => {
+  const [showForm, setShowForm] = useState(false);
+
   const [preferenceDetails, setPreferenceDetails] = useState({
     game_id: 1,
     mic_required: false,
@@ -25,39 +28,48 @@ const SessionForm = () => {
     createSession(preferenceDetails);
   };
 
+  const toggleForm = () => {
+    setShowForm(prevState => !prevState);
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Title:
-        <input type="text" name="title" value={preferenceDetails.title} onChange={handleInputChange} />
-      </label>
-      <br />
-      <label>
-        Description:
-        <input type="text" name="description" value={preferenceDetails.description} onChange={handleInputChange} />
-      </label>
-      <br />
-      <label>
-        Game ID:
-        <input type="number" name="game_id" value={preferenceDetails.game_id} onChange={handleInputChange} />
-      </label>
-      <label>
-        Mic Required:
-        <input type="checkbox" name="mic_required" checked={preferenceDetails.mic_required} onChange={handleInputChange} />
-      </label>
-      <br />
-      <label>
-        Max Players:
-        <input type="number" name="max_players" value={preferenceDetails.max_players} onChange={handleInputChange} />
-      </label>
-      <br />
-      <label>
-        Discord Link:
-        <input type="text" name="discord_link" value={preferenceDetails.discord_link} onChange={handleInputChange} />
-      </label>
-      <br />
-      <button type="submit">Create Session</button>
-    </form>
+    <div className='form-div'>
+     <button className="session-form-button" onClick={toggleForm}>Create a new session</button>
+      {showForm && (
+        <form className='session-form' onSubmit={handleSubmit}>
+          <label>
+            Title:
+            <input type="text" name="title" value={preferenceDetails.title} onChange={handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Description:
+            <input type="text" name="description" value={preferenceDetails.description} onChange={handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Game ID:
+            <input type="number" name="game_id" value={preferenceDetails.game_id} onChange={handleInputChange} />
+          </label>
+          <label>
+            Mic Required:
+            <input type="checkbox" name="mic_required" checked={preferenceDetails.mic_required} onChange={handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Max Players:
+            <input type="number" name="max_players" value={preferenceDetails.max_players} onChange={handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Discord Link:
+            <input type="text" name="discord_link" value={preferenceDetails.discord_link} onChange={handleInputChange} />
+          </label>
+          <br />
+          <button type="submit">Create Session</button>
+        </form>
+      )}
+    </div>
   );
 };
 
