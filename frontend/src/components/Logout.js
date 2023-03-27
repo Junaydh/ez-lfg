a01e1e96a01a352f0f7ab973c94f0bcd3a5aa193
Junayd H <junaydhus@gmail.com>
2023-03-27 19:00:43 -0400

    remove hook

--- frontend/src/components/Logout.js
+++ frontend/src/components/Logout.js
@@ -1,11 +1,18 @@
 import React, { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
 import { AuthContext } from '../contexts/auth';
 import { logout } from '../services/auth';
 
 const Logout = () => {
   const { updateUser } = useContext(AuthContext);
-  const history = useHistory();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      updateUser(null);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <button onClick={handleLogout}>Logout</button>
