9863969d8d5708668c4ef975f48df999ad08b2e8
Adam-Marx <adamalexmarx@gmail.com>
2023-03-26 19:53:03 -0700

    messing around with hardcoded user id

--- frontend/src/hooks/createSession.js
+++ frontend/src/hooks/createSession.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function createSession (preferenceDetails) {
   const userId = 8; // Replace with your actual user ID
-  axios.post(`/sessions/create/host/${userId}`, preferenceDetails)
+  axios.post(`http://localhost:3001/sessions/create/host/${userId}`, preferenceDetails)
     .then(response => {
       console.log(response.data);
     })
