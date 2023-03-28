214a08aaa20c10a37d6631dc518160944683a484
Adam-Marx <adamalexmarx@gmail.com>
2023-03-27 20:24:57 -0700

    Import client from discordBot.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require("cors");
-const indexRouter = require('./routes/index');
-const gamesRouter = require('./routes/gamesApi');
+const client = require('./discordBot');
+
 
 const app = express();   
 var usersRouter = require('./routes/users');
 const sessionsRouter = require('./routes/sessionsApi');
+const indexRouter = require('./routes/index');
+const gamesRouter = require('./routes/gamesApi');
+
+client.login(process.env.TOKEN);
 
 
 // view engine setup
