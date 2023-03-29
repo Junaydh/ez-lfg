aa89b78f02530cdb4aab2c1889cdf1e76efa141c
Adam-Marx <adamalexmarx@gmail.com>
2023-03-29 05:30:17 -0700

    better pictures for seeds

--- backend/db/seeds/4_sessions_users_seeds.sql
+++ backend/db/seeds/4_sessions_users_seeds.sql
@@ -3,7 +3,7 @@ TRUNCATE TABLE sessions_users RESTART IDENTITY CASCADE;
 INSERT INTO sessions_users (user_id, session_id)
 VALUES (1, 1),
        (2, 1),
-       (3, 2),
+       (2, 2),
        (4, 2),
        (5, 3),
        (6, 3);
