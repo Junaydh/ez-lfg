5bcfac120c078ab039a44776e4a64f92329427ff
Adam-Marx <adamalexmarx@gmail.com>
2023-03-29 00:01:35 -0700

    Move login register logic out of navbar and into seperate pages

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,33 +1,11 @@
+// Navbar.js
 import React, { useContext, useState } from 'react';
 import './Navbar.scss';
-import Login from './Login';
-import Logout from './Logout';
-import Register from './Register';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../contexts/auth';
 
-
-function Navbar() {
-  const [showRegistration, setShowRegistration] = useState(false);
-  const [error, setError] = useState('');
+export default function Navbar() {
   const { user } = useContext(AuthContext);
-
-  const renderLogin = () => {
-    if (user) {
-      return (
-        <div>
-          <Logout />
-        </div>
-      )
-      } else {
-      return (
-        <div>
-          <Login setError={setError}/>
-          {showRegistration && <button onClick={() => setShowRegistration(false)}>Close Form</button>}
-          {!showRegistration && <button onClick={() => setShowRegistration(true)}>Register</button>}
-        </div>
-      )
-    }
-  }
 
   const publicUrl = process.env.PUBLIC_URL;
 
@@ -36,22 +14,19 @@ function Navbar() {
       <img src={`${publicUrl}/EZLFG.png`} alt="logo" className="logo" />
       <ul className="navigation-links">
         <div>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-        </div>
-        <div>
-          <li>{renderLogin()}</li>
-        </div>
-        <div>
-          <li>{showRegistration && <Register setShowRegistration={setShowRegistration} setError={setError} />}</li>
+          {user ? (
+            <div>
+              <Link to="/profile">Profile</Link>
+            </div>
+          ) : (
+            <div>
+              <Link to="/">Home</Link>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </div>
+          )}
         </div>
       </ul>
-      <div className="auth-buttons">
-        {error && <div className="error">{error}</div>}
-      </div>
     </nav>
   );  
 }
-
-export default Navbar;
