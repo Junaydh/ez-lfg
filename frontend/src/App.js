1ef5f1064eedf74528ee6ed79e3c7f6ab08cbd84
Colton Hyland <colthyland@gmail.com>
2023-03-29 17:33:49 -0700

    Development example for hosting/new layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
-import './App.scss';
-import Navbar from './components/Navbar';
-import GamesList from './components/GamesList';
-import FilterBar from './components/Filterbar';
-import UserList from './components/UserList';
-import { useSessions } from './hooks/useSessions';
-import SessionList from './components/SessionList';
-import SessionForm from './components/SessionForm';
-import React, { useState, useContext, useEffect } from 'react';
-import { AuthContext } from './contexts/auth';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/pages/LoginPage';
-import RegisterPage from './components/pages/RegisterPage';
+import logo from "./logo.svg";
+import "./App.scss";
+import Navbar from "./components/Navbar";
+import GamesList from "./components/GamesList";
+import FilterBar from "./components/Filterbar";
+import UserList from "./components/UserList";
+import { useSessions } from "./hooks/useSessions";
+import SessionList from "./components/SessionList";
+import SessionForm from "./components/SessionForm";
+import React, { useState, useContext, useEffect } from "react";
+import { AuthContext } from "./contexts/auth";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/pages/LoginPage";
+import RegisterPage from "./components/pages/RegisterPage";
 
 function App() {
   const [selectedGameId, setSelectedGameId] = useState(null);
@@ -30,47 +30,79 @@ function App() {
   }
 
   useEffect(() => {
-    if(!showForm){
+    if (!showForm) {
       fetchSessions();
     }
-  }, [showForm])
+  }, [showForm]);
 
   const handleGameClick = (gameId, gameCover) => {
-    console.log(gameCover)
+    console.log(gameCover);
 
     //rerite to look like lines 38-42
-    setSelectedGameId(prevSelectedGameId => prevSelectedGameId === gameId ? null : gameId);
+    setSelectedGameId((prevSelectedGameId) =>
+      prevSelectedGameId === gameId ? null : gameId
+    );
 
-    if(gameCover === selectedGameCover) {
+    if (gameCover === selectedGameCover) {
       setSelectedGameCover(null);
     } else {
       setSelectedGameCover(gameCover);
     }
-    
-
   };
 
   return (
     <Router>
-      <main style={{backgroundImage: `url(${selectedGameCover})` }}>
+      <main style={{
+        backgroundImage: `url(${selectedGameCover})`,
+         backgroundSize: "cover",
+        backgroundPosition: "center center",
+        }}>
         <Navbar />
         <Routes>
-          <Route path="/" element={
-            <div>
-              <div className='games-list'>
-                <GamesList selectedGameId={selectedGameId} onGameClick={handleGameClick} />
-              </div>
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="create-session">
 
-              <div className='create-session'> 
-              {!showForm && <button className="session-form-button" onClick={() => setShowForm(!showForm)}>Create a new session</button>}
-              {showForm && <button className="session-form-button-cancel" onClick={() => setShowForm(!showForm)}>Cancel</button>}
-              </div>
-                {showForm && <SessionForm setShowForm={setShowForm}/>}
-              
-              
-              <SessionList gameCover ={selectedGameCover} sessions={sessions} userId={userId}/> 
-            </div>
-          } />
+                  {showForm && (
+                    <button
+                      className="session-form-button-cancel"
+                      onClick={() => setShowForm(!showForm)}
+                    >
+                      Cancel
+                    </button>
+                  )}
+                </div>
+                {showForm && <SessionForm setShowForm={setShowForm} />}
+                <div className="app-content-wrapper">
+                  <div className="app-content">
+                    <div className="games-list">
+                    {!showForm && (
+                    <button
+                      className="session-form-button"
+                      onClick={() => setShowForm(!showForm)}
+                    >
+                      Create a new session
+                    </button>
+                  )}
+                      <GamesList
+                        selectedGameId={selectedGameId}
+                        onGameClick={handleGameClick}
+                      />
+                    </div>
+                    <div className="sessions-list">
+                      <SessionList
+                        gameCover={selectedGameCover}
+                        sessions={sessions}
+                        userId={userId}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </>
+            }
+          />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
@@ -78,6 +110,5 @@ function App() {
     </Router>
   );
 }
-
 
 export default App;
--- frontend/src/components/SessionListItem.js
+++ frontend/src/components/SessionListItem.js
@@ -1,28 +1,27 @@
-import './SessionListItem.scss';
-import { joinSession, leaveSession } from '../hooks/joinOrLeaveSession';
-import { getSessionPlayers } from '../hooks/getSessionPlayers';
-import { useState, useEffect } from 'react';
-import { kickPlayer } from '../hooks/kickPlayer';
-
+import "./SessionListItem.scss";
+import { joinSession, leaveSession } from "../hooks/joinOrLeaveSession";
+import { getSessionPlayers } from "../hooks/getSessionPlayers";
+import { useState, useEffect } from "react";
+import { kickPlayer } from "../hooks/kickPlayer";
 
 function SessionListItem({ session, userId }) {
   const [sessionPlayers, setSessionPlayers] = useState([]);
   const [joined, setJoined] = useState(false);
-  
+
   useEffect(() => {
-    getSessionPlayers(session.id).then(players => {
+    getSessionPlayers(session.id).then((players) => {
       setSessionPlayers(players);
     });
   }, [session.id]);
-  
+
   useEffect(() => {
-    if (sessionPlayers.some(user => user.id === userId)) {
+    if (sessionPlayers.some((user) => user.id === userId)) {
       setJoined(true);
     } else {
       setJoined(false);
     }
-  }, [sessionPlayers, userId]);  
-  
+  }, [sessionPlayers, userId]);
+
   const date = new Date(session.created_at);
   const formattedDate = date.toLocaleDateString("en-US", {
     year: "numeric",
@@ -34,26 +33,24 @@ function SessionListItem({ session, userId }) {
     hour12: true,
   });
 
- 
   const handleKick = (playerId) => {
     kickPlayer(session.id, playerId).then(() => {
-      getSessionPlayers(session.id).then(players => {
-        setSessionPlayers(players)
-        .catch(err => {
-          return err.message
-        })
+      getSessionPlayers(session.id).then((players) => {
+        setSessionPlayers(players).catch((err) => {
+          return err.message;
+        });
       });
     });
-  }; 
+  };
 
   const isCreator = session.creator.id === userId;
-  
+
   const playerUsernames = (
     <div className="player-details">
       {sessionPlayers.length === 0 ? (
         <div className="empty-session">Session is empty</div>
       ) : (
-        <table className='player-list-table'>
+        <table className="player-list-table">
           <thead>
             <tr>
               <th></th>
@@ -66,14 +63,23 @@ function SessionListItem({ session, userId }) {
             {sessionPlayers.map((player) => (
               <tr key={player.id}>
                 <td>
-                  <img src={player.profile_pic} alt="Profile" className='profile-pic' />
+                  <img
+                    src={player.profile_pic}
+                    alt="Profile"
+                    className="profile-pic"
+                  />
                 </td>
                 <td>{player.username}</td>
                 <td>{player.discord_tag}</td>
                 {isCreator && (
                   <td>
                     {player.id !== userId && (
-                      <button className='kick-button' onClick={() => handleKick(player.id)}>Kick</button>
+                      <button
+                        className="kick-button"
+                        onClick={() => handleKick(player.id)}
+                      >
+                        Kick
+                      </button>
                     )}
                   </td>
                 )}
@@ -85,84 +91,89 @@ function SessionListItem({ session, userId }) {
     </div>
   );
 
- 
-  
   const handleJoinOrLeaveSession = () => {
     if (sessionPlayers.length >= session.max_players) {
       return; // Don't allow joining when session is full
     }
     if (userId === null) {
-      alert("Please log in to join a session")
+      alert("Please log in to join a session");
       return; // Don't allow joining when user is not logged in
     }
-  
+
     if (joined) {
       // User is already in the session, render Leave Session button
       leaveSession(userId, session.id).then(() => {
-        getSessionPlayers(session.id).then(players => {
+        getSessionPlayers(session.id).then((players) => {
           setSessionPlayers(players);
           setJoined(false); // update the joined state to false
         });
       });
-    } else if (sessionPlayers.some(user => user.id === userId)) {
+    } else if (sessionPlayers.some((user) => user.id === userId)) {
       // User is already in the session but not joined, render Join Session + button
       setJoined(true); // update the joined state to true
     } else {
       // User is not in the session, render Join Session + button
       joinSession(userId, session.id).then(() => {
-        getSessionPlayers(session.id).then(players => {
+        getSessionPlayers(session.id).then((players) => {
           setSessionPlayers(players);
           setJoined(true); // update the joined state to true
         });
       });
     }
   };
-  
 
-return (
-      <div key={session.id} className="session-card">
-      <h2>{session.title}</h2>
-      <div className='host-description'>
-        <div className='host-details'>
-          <h3>Session #{session.id}</h3>
-          <h3>Host: {session.creator.username}</h3>
-          <h5>
-            <i className="fab fa-discord"></i> {session.creator.discord_tag}
-          </h5>
-        </div>
+  return (
+    <div key={session.id} className="session-card-wrapper">
+        <h2>{session.title}</h2>
         <p>{session.description}</p>
-      </div>
-      <div className="details">
-        <div className="preferences">
-          {/*sessionPlayers.length === 0 ? (<div className="players"><p>Session is empty</p></div>) : <></>*/}
-        <span>
-          Players: {sessionPlayers.length}/{session.max_players}
-        </span>
-          <span>Mic Required: {session.mic_required ? "Yes" : "No"}</span>
-          <span>Playstyle: {session.competitve ? "Competitive" : "Casual"} </span>
-          <span>Region: {session.region}</span>
-          <span>Platform: {session.platform}</span>
-        </div>  
-        {sessionPlayers.length === 0 ? (
+      <div key={session.id} className="session-card">
+        <div className="host-description">
+          <div className="host-details">
+            <h3>Session </h3>
+            <p>{session.id}</p>
+            <h3>Host</h3>
+            <p>{session.creator.username}</p>
+            <h5>
+              <i className="fab fa-discord"></i> {session.creator.discord_tag}
+            </h5>
+          </div>
+        </div>
+        <div className="details">
+          <div className="preferences">
+            {/*sessionPlayers.length === 0 ? (<div className="players"><p>Session is empty</p></div>) : <></>*/}
+            <div>
+            <h3>Players</h3> 
+            <p>{sessionPlayers.length}/{session.max_players}</p>
+            </div>
+            <div>Mic Required: {session.mic_required ? "Yes" : "No"}</div>
+            <div>
+              Playstyle: {session.competitve ? "Competitive" : "Casual"}{" "}
+            </div>
+            <div>Region: {session.region}</div>
+            <div>Platform: {session.platform}</div>
+          </div>
+          {sessionPlayers.length === 0 ? (
             <div className="session-empty">Session is empty</div>
           ) : (
-            <div className="players-container">
-              {playerUsernames}
-          </div>        
+            <div className="players-container">{playerUsernames}</div>
           )}
+        </div>
+        <footer>
+          <span>{formattedDate}</span>
+          {sessionPlayers.length >= session.max_players ? (
+            <span className="session-full">Session Full</span>
+          ) : joined ? (
+            <button
+              className="leave-session"
+              onClick={handleJoinOrLeaveSession}
+            >
+              Leave Session
+            </button>
+          ) : (
+            <button onClick={handleJoinOrLeaveSession}>Join Session +</button>
+          )}
+        </footer>
       </div>
-      <footer>
-        <span>{formattedDate}</span>
-        {sessionPlayers.length >= session.max_players ? (
-          <span className="session-full">Session Full</span>
-        ) : joined ? (
-          <button className='leave-session' onClick={handleJoinOrLeaveSession}>
-            Leave Session
-          </button>
-        ) : (
-          <button onClick={handleJoinOrLeaveSession}>Join Session +</button>
-        )}
-      </footer>
     </div>
   );
 }
