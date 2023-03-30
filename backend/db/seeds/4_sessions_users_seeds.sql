1670e2dbbbcd7be1fe1d7e98ad25bc5bc7dfdf67
Junayd H <junaydhus@gmail.com>
2023-03-29 21:56:35 -0400

    fix session seeds

--- backend/db/seeds/4_sessions_users_seeds.sql
+++ backend/db/seeds/4_sessions_users_seeds.sql
@@ -4,6 +4,9 @@ INSERT INTO sessions_users (user_id, session_id)
 VALUES (1, 1),
        (2, 1),
        (2, 2),
+       (4, 4)
        (4, 2),
+       (5, 5),
        (5, 3),
-       (6, 12);
+       (6, 12),
+       (6, 6);
